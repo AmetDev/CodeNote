d8d6217d3cf48838173d30847fda85ddfc975a02
AmetDev <ametsejdaliev@gmail.com>
2023-10-04 21:02:43 +0300

    server-01-express-mariadb

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+// Use the MariaDB Node.js Connector
+var mariadb = require('mariadb')
+
+// Create a connection pool
+var pool = mariadb.createPool({
+	host: '127.0.0.1',
+	port: 3306,
+	user: 'app_user',
+	password: 'Password123!',
+	database: 'todo',
+})
+
+// Expose a method to establish connection with MariaDB SkySQL
+module.exports = Object.freeze({
+	pool: pool,
+})

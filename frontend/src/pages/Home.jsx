1fca1d3478f45ca98c90aecd4077534aadcb7214
Dash <dasha.semivolos@gmail.com>
2023-10-27 21:56:37 +0300

    27.10

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import SearchBar from "../Components/Search";
 import Navbar from "../Components/Navbar";
+import Card from "../Components/Cards";
+import '../styles/Home.css';
 function Home() {
     
     return (
       <div className="home">
         <div className="container">
-           <div>
-                {<Navbar />}
-           </div>
-            <div>
+            <div className="search">
                 {<SearchBar />}
+            </div>
+            <div className="navbar">
+                {<Navbar />}
+            </div>
+            <div className="div_cards">
+              {<Card />}
             </div>
         </div>
       </div>

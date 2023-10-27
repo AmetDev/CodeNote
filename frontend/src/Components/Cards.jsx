a0b7be0b498a0987d91b4f667ba4dc890dd2a0d7
Dash <dasha.semivolos@gmail.com>
2023-10-27 21:58:32 +0300

    add_cards

---
+++ frontend/src/Components/Cards.jsx
@@ -0,0 +1,41 @@
+function Card () {
+    return (
+        <div className="cards">
+            <div className="card">
+                <h4>JAVASCIRPT High level language</h4>
+                <p>JS - the popular programming language in the world. He is multiparadigm and event language. Why you need to learn JS?</p>
+            </div>
+
+            <div className="card">
+                <h4>JAVASCIRPT High level language</h4>
+                <p>JS - the popular programming language in the world. He is multiparadigm and event language. Why you need to learn JS?</p>
+            </div>
+            
+            <div className="card">
+                <h4>JAVASCIRPT High level language</h4>
+                <p>JS - the popular programming language in the world. He is multiparadigm and event language. Why you need to learn JS?</p>
+            </div>
+            
+            <div className="card">
+                <h4>JAVASCIRPT High level language</h4>
+                <p>JS - the popular programming language in the world. He is multiparadigm and event language. Why you need to learn JS?</p>
+            </div>
+            
+            <div className="card">
+                <h4>JAVASCIRPT High level language</h4>
+                <p>JS - the popular programming language in the world. He is multiparadigm and event language. Why you need to learn JS?</p>
+            </div>
+
+            <div className="card">
+                <h4>JAVASCIRPT High level language</h4>
+                <p>JS - the popular programming language in the world. He is multiparadigm and event language. Why you need to learn JS?</p>
+            </div>
+
+            <div className="card">
+                <h4>JAVASCIRPT High level language</h4>
+                <p>JS - the popular programming language in the world. He is multiparadigm and event language. Why you need to learn JS?</p>
+            </div>
+        </div>
+    )
+}
+export default Card;
